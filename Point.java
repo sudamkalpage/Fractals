b39ef800c46baf0a4b20b394523f0c0812b5bf2d
sudamkalpage <58980965+sudamkalpage@users.noreply.github.com>
2020-11-21 00:11:02 +0530

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,18 @@
+class Point { // working with point objects is easier
+
+	private double x, y;
+
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() { // x coordinate
+		return this.x;
+	}
+
+	public double getY() {// y coordinate
+		return this.y;
+	}
+
+}
